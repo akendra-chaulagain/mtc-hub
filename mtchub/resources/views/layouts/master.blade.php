3742c97f689471587ab76dca5aeacf3410476209
Akendra Chaulagain <yakendrachaulagain27@gmail.com>
2022-10-10 10:40:30 +0545

    a

--- mtchub/resources/views/layouts/master.blade.php
+++ mtchub/resources/views/layouts/master.blade.php
@@ -156,7 +156,7 @@ if (isset($normal)) {
                         <div id="menu" class="text-left ">
                             <ul class="offcanvas_main_menu">
                                  <li>
-                                            <a class="active" href="https://mtc.demo.radiantnepal.com/mtchub/">Home</a>
+                                            <a  href="https://mtc.demo.radiantnepal.com/mtchub/">Home</a>
                                         </li>
                                
                                
